7ee654757d23460355d96f4bb100b60abe13d11a
Nicoli Rama Fraccanabbia <nicolirama@hotmail.com>
2023-09-26 18:37:51 -0300

    Criando outros componentes da página de detalhes

--- dscommerce-frontend/src/App.tsx
+++ dscommerce-frontend/src/App.tsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import ButtonInverse from './components/ButtonInverse';
+import ButtonPrimary from './components/ButtonPrimary';
+import HeaderClient from './components/HeaderClient';
+import ProductDetailsCard from './components/ProductDetailsCard';
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
 
-  return (
+  return(
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <HeaderClient/>
+    <main>
+      <section id="product-details-section" className ="dsc-container">
+        <ProductDetailsCard />
+        <div className ="dsc-btn-page-container">
+            <ButtonPrimary />
+            <ButtonInverse />
+        </div>
+      </section>
+    </main> 
     </>
-  )
+  );
 }
 
-export default App
